0ebba686e1486b9248c99b7eeb5c1616348b124e
Roland <117606042+rcfisch@users.noreply.github.com>
2024-10-20 21:01:10 -0500

    Added Build Artifacts

--- src/main/java/com/rolandfisch/sumberMinigame/SumberMinigame.java
+++ src/main/java/com/rolandfisch/sumberMinigame/SumberMinigame.java
@@ -1,5 +1,6 @@
 package com.rolandfisch.sumberMinigame;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SumberMinigame extends JavaPlugin {
@@ -7,11 +8,11 @@ public final class SumberMinigame extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Sumber Enabled (from plugin)");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Sumber Enabled (from plugin)");
     }
 }
